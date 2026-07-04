c44ccc0d391fd37e84f2a04e8ccc1a5fb9e0eea6
Othniel-coder <othniasante@gmail.com>
2026-07-04 12:31:45 +0000

    Added Github Actions CI workflow

---
+++ test_app.py
@@ -0,0 +1,11 @@
+import subprocess
+
+result = subprocess.run(
+     ["python", "app.py"],
+     capture_output=True, 
+     text=True
+     ) 
+     
+assert result.returncode == 0 
+assert "Cloud CI Pipeline Running" in result.stdout 
+print("Test Passed")
